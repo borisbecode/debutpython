0ec03f0883fb81494cde4f4bbc2546a6e153a31b
borisbecode <boris.lorenzonetto@gmail.com>
2021-12-15 15:03:50 +0100

    ok

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+url = "https://www.gov.uk/search/news-and-communications"
+page = requests.get(url)
+# Voir le code html source
+print(page.content)
+
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.gov.uk/search/news-and-communications"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
